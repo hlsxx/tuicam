eda985ea3d15e48b735b07e30d1f0bd7f590c194
hlsxx <holespato@gmail.com>
2024-12-23 14:28:56 +0100

    Layout

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
 use crossterm::event::KeyCode;
-use ratatui::{layout::{Alignment, Constraint, Flex, Layout}, style::{Color, Style}, text::Line, widgets::{Block, BorderType, Clear, Paragraph}, DefaultTerminal};
+use ratatui::{layout::{Alignment, Constraint, Direction, Flex, Layout}, style::{Color, Style, Stylize}, text::{Line, Span, Text}, widgets::{Block, BorderType, Clear, Paragraph}, DefaultTerminal};
 
 use crate::frame_handler::FrameHandler;
 pub const SCALE_FACTOR: u16 = 2;
@@ -31,6 +31,8 @@ impl<'a> App<'a> {
   ///
   /// Renders widgets into frames
   pub async fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+    let primary_color = Color::Rgb(168, 50, 62);
+
     loop {
       let terminal_size = self.terminal.size()?;
       let mut frame_buffer = String::new();
@@ -47,8 +49,16 @@ impl<'a> App<'a> {
       self.terminal.draw(|frame| {
         let area = frame.area();
 
+        let chunks = Layout::new(Direction::Vertical, [
+          Constraint::Percentage(100),
+          Constraint::Length(5)
+        ]).split(area);
+
+        let top_chunk = chunks[0];
+        let bottom_chunk = chunks[1];
+
         let block = Block::bordered()
-          .border_style(Style::default().fg(Color::Rgb(168, 50, 62)))
+          .border_style(Style::default().fg(primary_color))
           .title_bottom(Line::from(" tui-cam-rs "))
           .title_style(Style::default())
           .title_alignment(Alignment::Center)
@@ -65,11 +75,25 @@ impl<'a> App<'a> {
         let vertical = Layout::vertical([Constraint::Length(terminal_size.height / SCALE_FACTOR)])
           .flex(Flex::Center);
 
-        let [area] = vertical.areas(area);
-        let [area] = horizontal.areas(area);
+        let [top_chunk] = vertical.areas(top_chunk);
+        let [top_chunk] = horizontal.areas(top_chunk);
 
-        frame.render_widget(Clear, area);
-        frame.render_widget(cam_paragraph, area);
+        let tools_text = Text::from(vec![
+          Line::from(vec![
+            Span::from("ESC").bold(),
+            Span::from(" exit | "),
+            Span::from("[ __ ]").bold(),
+            Span::from(" switch mode")
+          ]).style(Style::default().fg(primary_color))
+        ]);
+
+        let tools_paragraph = Paragraph::new(tools_text)
+          .alignment(Alignment::Center)
+          .centered();
+
+        frame.render_widget(Clear, top_chunk);
+        frame.render_widget(cam_paragraph, top_chunk);
+        frame.render_widget(tools_paragraph, bottom_chunk);
       })?;
     }
 
